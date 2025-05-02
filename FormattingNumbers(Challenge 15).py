9ed27a61606d80ce8ffab2f359abbc9c2bd0fbe9
User-Aden <mus0035@copperfieldcollege.vic.edu.au>
2025-05-02 15:11:46 +1000

    FormattingNumbers(Challenge 15).py

---
+++ FormattingNumbers(Challenge 15).py
@@ -0,0 +1,12 @@
+nOne = 3.123456789
+nTwo = 9.87654321
+#Times nOne by nTwo
+ans =(nOne*nTwo)
+#Print nOne times nTwo
+print (ans)
+#rounds the answer to 3 dps
+print (str(ans)[:5])
+#adding a zero to a decimal
+n3 =1.1
+#Print answer into two decimal places
+print ("%0.2f" % (n3))
