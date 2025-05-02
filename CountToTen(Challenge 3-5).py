fe71cfa4b51138eb26b856f4c671adfcc6e11101
User-Aden <mus0035@copperfieldcollege.vic.edu.au>
2025-05-02 14:35:00 +1000

    CountToTen(Challenge 3-5).py

---
+++ CountToTen(Challenge 3-5).py
@@ -0,0 +1,8 @@
+#This code is for challege tasks 3 to 5. (Write a program that... and Change the Program)
+
+#Ask for a number between 0 and 10 (Can be decimal)
+x = float(input("Enter a number between 0 and 10: "))
+#Ask for another number between 0 and 10 (Can be decimal)
+d = float(input("Enter a number between 0 and 10: "))
+#Add the two numbers together
+print(x + d)
