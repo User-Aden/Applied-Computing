6240216c124f4fbd2cc0b98c8ebf8f7d02db3be8
User-Aden <mus0035@copperfieldcollege.vic.edu.au>
2025-05-03 12:34:30 +1000

    Mobile_Phone_Cost(Challenge 23).py

--- Mobile_Phone_Cost(Challenge 23).py
+++ Mobile_Phone_Cost(Challenge 23).py
@@ -1,4 +1,5 @@
 import time
+import math
 #Ask user how many pictures they would like to send this month
 x = float(input("How many pictures would you like to send this month (they cost $0.35 each): "))
 #Pause for 0.5 seconds
@@ -8,18 +9,19 @@ d = float(input("How many texts would you like to send this month (they cost $0.
 #Pause for 0.5 seconds
 time.sleep(0.5)
 #Ask user how many data they would like to send this month
-c = float(input("How much data would you like to use this month (they cost $0.05 for 1 MB): "))
+c = float(input("How much data would you like to use this month ( in MBs): "))
 #Pause for 0.5 seconds
 time.sleep(0.5)
-#Calculate the cost of every price: times the amount of picutres, texts, and data by their espective prices
+#Calculate the cost of every price: times the amount of picutres, texts, and data by their respective prices
 pic = x * 0.35
 text = d * 0.10
-data = c * 0.05
+data500MB = math.ceil(c / 500)
+data = data500MB * 2.50 
 #Add all the prices up
 bill = pic + text + data
 #Print the cost of the bill
-print ("\nThe total cost of your phone bills this month comes to $" + str(bill))
-#Pause for 0.5 seconds
+print ("\nThe total cost of your phone bills this month comes to $" + str(round(bill,2)))
+#Pause for 0.75 seconds
 time.sleep(0.75)
 #If the bill is greater than $10.0, suggest the contract
 if bill > 10:
