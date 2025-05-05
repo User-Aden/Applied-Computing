88b915ec97a53cda55a39a5432361032adba4730
User-Aden <mus0035@copperfieldcollege.vic.edu.au>
2025-05-05 18:41:16 +1000

    For Loop(Challenge 26).py

--- For Loop(Challenge 26).py
+++ For Loop(Challenge 26).py
@@ -1,4 +1,4 @@
-#Loop through the selected numbers, starting from 10 up to 105, counting by 5 each time
+#Loop through the selected numbers, starting from 10 up to (but not including) 105, counting by 5 each time - 05/05/25
 for x in range(10, 105, 5):
-#Print x until x is false.
+#Print the current value of x - 05/05/25
     print(x)
