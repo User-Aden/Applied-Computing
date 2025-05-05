6e1995cb2c8a6ffd0dbec1d348213e5c7ee54c30
User-Aden <mus0035@copperfieldcollege.vic.edu.au>
2025-05-05 18:46:54 +1000

    Challenge 29(Challenge 29).py

--- Challenge 29(Challenge 29).py
+++ Challenge 29(Challenge 29).py
@@ -1,8 +1,8 @@
-#Set the starting number to 100
+#Set the starting number to 100 - 05/05/25
 number = 100
-#Whil the number is greater than 10 or less than 1
+#While the number is greater than 10 or less than 1 - 05/05/25
 while number > 10 or number < 1:
-#Ask for a number that is between 1 and 10
+    #Ask for a number that is between 1 and 10 - 05/05/25
     number = int(input("Please enter a number between 1 and 10: "))
-#Print the number multiplied by 10
+#Print the number multiplied by 10 - 05/05/25
 print("Your number multiplied by 10 is:", number * 10)
