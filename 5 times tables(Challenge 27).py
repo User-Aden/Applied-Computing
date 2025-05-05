7858f4f88c9f0b1c881e8d12347c758d876906c6
User-Aden <mus0035@copperfieldcollege.vic.edu.au>
2025-05-05 18:44:00 +1000

    5 times tables(Challenge 27).py

--- 5 times tables(Challenge 27).py
+++ 5 times tables(Challenge 27).py
@@ -1,6 +1,6 @@
-#Print the title of: Five Times Table
+#Print the title of: Five Times Table - 05/05/25
 print('Five Times Table:')
-#Loop through numbers from 0 to 12
+#Loop through numbers from 0 to 12 - 05/05/25
 for x in range(0, 13):
-#Print all of the times tables, numbers 0-12 go in f"{x}, x 5 =, and the answer to the five times tables x times 5.
+    #Print all of the times tables, numbers 0-12 go in f"{x}, x 5 =, and the answer to the five times tables x times 5. - 05/05/25
     print(f"{x} x 5 = {x * 5}")
