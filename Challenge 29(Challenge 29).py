e64febc4fa070ec64e963cd553e70a364b96e403
User-Aden <mus0035@copperfieldcollege.vic.edu.au>
2025-05-05 18:05:08 +1000

    Challenge 29(Challenge 29).py

---
+++ Challenge 29(Challenge 29).py
@@ -0,0 +1,8 @@
+#Set the starting number to 100
+number = 100
+#Whil the number is greater than 10 or less than 1
+while number > 10 or number < 1:
+#Ask for a number that is between 1 and 10
+    number = int(input("Please enter a number between 1 and 10: "))
+#Print the number multiplied by 10
+print("Your number multiplied by 10 is:", number * 10)
