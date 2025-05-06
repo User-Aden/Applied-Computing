0d16cf93a38d6ef45e4dae5d94d035596d3b4302
User-Aden <mus0035@copperfieldcollege.vic.edu.au>
2025-05-06 11:57:52 +1000

    Age(Challenge 8).py

--- Age(Challenge 8).py
+++ Age(Challenge 8).py
@@ -5,16 +5,11 @@ x = str(input("What is your name: "))
 time.sleep(0.5)
 #Ask for user's age
 d = int(input("How old are you: "))
-#Values of each time measurement abriviated 
-diy = 365
-hid = 24
-mih = 60
-sim = 60
 #Equations to find specific times.
-days_alive = d * diy
-hours_alive = days_alive * hid
-minutes_alive = hours_alive * mih
-seconds_alive = minutes_alive * sim
+days_alive = d * 365
+hours_alive = days_alive * 24
+minutes_alive = hours_alive * 60
+seconds_alive = minutes_alive * 60
 #Print user's name and say how many days, hours, minutes, and seconds they have been alive for
 print( str(x) + ", you are: ")
 print( f"{days_alive} days")
