f152abe74dfaf1cf5f54e5a10218a990c5689204
User-Aden <mus0035@copperfieldcollege.vic.edu.au>
2025-05-10 13:50:52 +1000

    (Triangle)MakingShapes(Challenge 34).py

--- (Triangle)MakingShapes(Challenge 34).py
+++ (Triangle)MakingShapes(Challenge 34).py
@@ -1,19 +1,19 @@
-import turtle 
+import turtle
 #Ask user what they would like the length of the sides to be
-size = int(input("Enter Length of Octagon..(Anything under 100 is small): "))
+size = int(input("Enter Length of Triangle..(Anything under 100 is small): "))
 #Set up the drawing window
 wn = turtle.Screen()
 #Create a turtle 
-tom = turtle.Turtle() 
-#Define a function that draws an octagon of the given size
-def Octagon(size):
-#Repeat steps 8 times as octagon has 8 sides
-    for a in range(8):
+tom = turtle.Turtle()
+#Define a function that draws an triangle of the given size
+def Triangle(size):
+#Repeat steps 3 times as octagon has 3 sides
+    for a in range (3):
 #Move the turtle by x amount
         tom.forward(size)
 #Turn the turtle 45 degrees to the right
-        tom.right(45)
+        tom.left(120)
 #Wait for a mouse or cursor click before closing the window
     wn.exitonclick()
-#Call the function to draw the octagon
-Octagon(size)
+#Call the function to draw the triangle
+Triangle(size)
