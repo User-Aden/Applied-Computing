75e40afd1b2c4d87f001bdb74ec19ab81d70c8c2
User-Aden <mus0035@copperfieldcollege.vic.edu.au>
2025-05-10 14:28:22 +1000

    (Octagon)MakingShapes(Challenge 34).py

--- (Octagon)MakingShapes(Challenge 34).py
+++ (Octagon)MakingShapes(Challenge 34).py
@@ -1,19 +1,19 @@
 import turtle 
-#Ask user what they would like the length of the sides to be
+#Ask user what they would like the length of the sides to be - 10/05/25
 size = int(input("Enter Length of Octagon..(Anything under 100 is small): "))
-#Set up the drawing window
+#Set up the drawing window - 10/05/25
 wn = turtle.Screen()
-#Create a turtle 
+#Create a turtle - 10/05/25
 tom = turtle.Turtle() 
-#Define a function that draws an octagon of the given size
+#Define a function that draws an octagon of the given size - 10/05/25
 def Octagon(size):
-#Repeat steps 8 times as octagon has 8 sides
+#Repeat steps 8 times as octagon has 8 sides - 10/05/25
     for a in range(8):
-#Move the turtle by x amount
+#Move the turtle by x amount - 10/05/25
         tom.forward(size)
-#Turn the turtle 45 degrees to the right
+#Turn the turtle 45 degrees to the right - 10/05/25
         tom.right(45)
-#Wait for a mouse or cursor click before closing the window
+#Wait for a mouse or cursor click before closing the window - 10/05/25
     wn.exitonclick()
-#Call the function to draw the octagon
+#Call the function to draw the octagon - 10/05/25
 Octagon(size)
