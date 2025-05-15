ac66730c116bcef0ef6139db724f12964cde5f01
User-Aden <mus0035@copperfieldcollege.vic.edu.au>
2025-05-15 13:03:47 +1000

    Positive_St(Challenge 37).py

---
+++ Positive_St(Challenge 37).py
@@ -0,0 +1,6 @@
+#Store the list of students in the variable of x - 15/05/25
+x = ['Andrew' , 'Bobby' , 'Renato' ,  'Tracey', 'Adam' ,  'Michael' , 'Nicci' ,  'Mary' , 'Lance' , 'Soula']
+#From the list pick a random choice - 15/05/25
+from random import choice
+#Print 'randomly chosen student' has been chosen for the prize! - 15/05/25
+print (choice(x)+ ' has been chosen for the prize!')
