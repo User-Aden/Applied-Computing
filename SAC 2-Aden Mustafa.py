fa9186057e334bb22d7010ed4ca79abdf8ad95a5
User-Aden <mus0035@copperfieldcollege.vic.edu.au>
2025-05-23 14:43:35 +1000

    SAC 2-Aden Mustafa.py

---
+++ SAC 2-Aden Mustafa.py
@@ -0,0 +1,17 @@
+import time
+noStudents = int(input('How many students are in your class: '))
+time.sleep(0.25)
+noPeriods = int(input('How many periods are held in a week: '))
+time.sleep(0.25)
+for n in range(1, noStudents + 1):
+    while True:
+        StuName = input(f'What is the name of student {n}?: ').strip().capitalize()
+
+
+time.sleep(0.25)
+for d in range(1, noPeriods + 1):
+    while True:
+        period = input(f'\nWas this student in period {d}? (P/A): ').strip().upper()
+        if period in ['P', 'A']:
+            break  
+
