54eaf6ba02cda60a69907704f3d88adf78270c04
User-Aden <mus0035@copperfieldcollege.vic.edu.au>
2025-06-05 10:19:14 +1000

    ShareingMoney(Challenge 12).py

--- ShareingMoney(Challenge 12).py
+++ ShareingMoney(Challenge 12).py
@@ -5,4 +5,5 @@ x = int(input("How many people do you want to share the money with (Including yo
 time.sleep(0.5)
 #Divide the $1000 dollars by how many people it is to be shared with, and print Each person will get x amount of dollars each
 money = 1000 / x
+#Print each person will get x amount of money
 print("Each person will get $" + str(money) + " each")
